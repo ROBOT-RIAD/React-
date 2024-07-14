04f007fcda2ae8c9740e9f1fb3313a460db14e6b
ROBOT-RIAD <riadhosain420@gmail.com>
2024-07-14 18:56:02 +0600

    first commit

---
+++ myapp/component.js
@@ -0,0 +1,14 @@
+ 
+ let a= 45;
+export let str ="riad"
+
+ let obj = {
+    name : "riad",
+    age : 27
+ };
+
+ let arr = [1,2,3,4];
+
+ let hello = ()=>{
+    console.log("helo riad");
+ }
---
+++ node js/script.js
@@ -0,0 +1,8 @@
+
+console.log("hello riad");
+
+let mat = require('date-format');
+
+let time =mat('hh:mm:ss.SSS',new Date());
+
+console.log(time);
